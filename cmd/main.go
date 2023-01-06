fe79ad603a99eb433ea27934cf5756c90b125a83
Timofey Ya <tim02timka@mail.ru>
2023-01-06 09:49:44 +0700

    Внедрение DI

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	todo "todo/study"
 	"todo/study/package/handler"
+	"todo/study/package/repository"
+	"todo/study/package/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handler := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 
 	err := srv.Run("2002", handler.InitRoutes())
